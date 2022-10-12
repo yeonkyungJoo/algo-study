17a36d6a62a0e523be62c2d0b7ee9ba441be935e
yeonkyung <younkyong94@gmail.com>
2022-10-12 18:22:00 +0900

    2022.10.12

--- coding_interview/12. graph/39. 코스 스케줄.py
+++ coding_interview/12. graph/39. 코스 스케줄.py
@@ -2,5 +2,36 @@ if __name__ == "__main__":
     n = 2
     course = [[1, 0], [0, 1]]
 
+    dic = dict()
+    for c in course:
+        start, end = c
+        if start not in dic:
+            dic[start] = []
+        dic[start].append(end)
+    # print(dic)
 
+    answer = True
+    traced = set()
+    visited = set()
+    def dfs(i):
+
+        if i in traced:
+            return False
+
+        if i in visited:
+            return True
+
+        traced.add(i)
+        for y in dic[i]:
+            if not dfs(y):
+                return False
+        traced.remove(i)
+        visited.add(i)
+
+        return True
+
+    for x in list(dic):
+        if not dfs(x):
+            answer = False
+    print(answer)
 
